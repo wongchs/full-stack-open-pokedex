999dc48c9c51c37b3505fc54d0201e23a9f3a841
wongchs <chyisiang8@gmail.com>
2025-01-20 10:10:10 +0800

    update

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 /* eslint-disable no-console */
 const express = require('express')
-const path = require('path')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
